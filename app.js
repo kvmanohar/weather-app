4b09a35ae6d1f4aa0c6e2400b3e3c89e93fec458
Manohar Kurapati <kvmanohar@gmail.com>
2018-07-29 10:33:20 +0100

    moved request call to seperate .js file

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const request = require('request');
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=42%20braodhurst%20place%20Basildon' +
-    '&key=' + process.env.WEATHER_APP_KEY,
-    json: true
-},(error,response, body) => {
-    // console.log(JSON.stringify(body,undefined,2));
-    console.log('Address: ', body.results[0].formatted_address);
-    console.log('Latitude: ', body.results[0].geometry.location.lat);
-    console.log('Longitude: ', body.results[0].geometry.location.lng);
-});
+const argv = yargs
+    .option({
+        a:{
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch the weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+console.log('Input address :', argv.address);    
+geocode.geocodeAddress(argv.address);
