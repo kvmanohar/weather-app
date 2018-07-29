266fe6495efe3cacaee6ae147cdcbf164cbc6c50
Manohar Kurapati <kvmanohar@gmail.com>
2018-07-29 10:52:11 +0100

    Replaced console logs with Callbacks

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-console.log('Input address :', argv.address);    
-geocode.geocodeAddress(argv.address);
+console.log('Input address :', argv.address);   
+ 
+geocode.geocodeAddress(argv.address,(errorMessage, results) => {
+    if (errorMessage){
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results,undefined,2));
+    }
+});
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
     request({
         url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURI(address) +
             '&key=' + process.env.WEATHER_APP_KEY,
         json: true
     }, (error, response, body) => {
         if (error) {
-            console.log('Unable to connect to Google Services');
+            callback('Unable to connect to Google service');
         } else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find that address');
+            callback('Unable to find that address');
         } else if (body.status === 'OK') {
-            console.log('Address: ', body.results[0].formatted_address);
-            console.log('Latitude: ', body.results[0].geometry.location.lat);
-            console.log('Longitude: ', body.results[0].geometry.location.lng);
+            callback(undefined,{
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
         }
         // console.log(JSON.stringify(body,undefined,2));
     });
